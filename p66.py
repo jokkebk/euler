fe34e76493b99bad9d419ad01d4f3d7dab95106e
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2015-02-18 15:11:06 +0200

    Fix problem 65 and 66 solutions&numbering

--- p66.py
+++ p66.py
@@ -1,14 +1,23 @@
 import math
 
-def square(n): return n == int(math.sqrt(n))**2
+def brahmagupta(N, a, b, k):
+    (best, M) = (False, 0)
 
-for D in range(2,1001):
-    if square(D): continue
-    solution = False
-    for x in range(2,10000000):
-        n = x*x - 1
-        if n % D or not square(n//D): continue
-        print("%d: %d" % (D, x))
-        solution = True
-        break
-    if not solution: print("No solution for %d!" % D)
+    for m in range(1,2*abs(k)+1):
+        if (a + b*m) % k: continue
+        val = abs(m*m - N)
+        if not best or val < best:
+            (best, M) = (val, m)
+        else:
+            break
+
+    return ((a*M+N*b)//abs(k), (a+b*M)//abs(k), (M*M-N)//k)
+
+def chakravala(N):
+    (a, b, k) = (1, 1, 1-N)
+    while k != 1: (a, b, k) = brahmagupta(N, a, b, k)
+    return a
+
+squares = {n*n for n in range(2,40)}
+
+print(max((chakravala(N), N) for N in range(2,1001) if N not in squares))
