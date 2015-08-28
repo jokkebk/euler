473e9a28bda6d89f68fd86e2b836177a2ce48c4b
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2015-08-28 11:26:36 +0300

    Optimized 110

--- p110.py
+++ p110.py
@@ -1,10 +1,10 @@
-import itertools
+from itertools import product
 from functools import reduce
 from operator import mul
 
 def gen():
     p, q = [2, 3, 5, 7], 11*13*17*19*23*29*31*37 
-    for a in itertools.product(range(5), repeat=4):
+    for a in product(range(5), repeat=4):
         if 3**8 * reduce(mul, [2*v+1 for v in a])//2+1 > 4*10**6:
             yield q * reduce(mul, [p[i]**v for i,v in enumerate(a)])
 
