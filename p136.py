dd55122178259a7610aa131feaf9b530edf906d0
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2015-09-08 15:33:46 +0300

    Problem 136 brute force

---
+++ p136.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from itertools import count
+import sys
+
+diff = lambda a, d: -a*a + 2*a*d + 3*d*d
+
+N = int(sys.argv[1])
+W, I = Counter(), 0
+last = 0 # Terminate after enough zeroes
+for a in count(1):
+    for d in count(2*a//6+1): # First diff(a,d)>0
+        v = diff(a,d)
+        if v >= N: break
+        W[v] += 1
+        I += 1
+        last = a
+    if last+2 < a: break
+
+print(I)
+print(sum(1 for i in W if W[i]==1))
