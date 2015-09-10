8fd792b2096ecece2264530f1285a67138b3f0dd
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2015-09-10 19:50:29 +0300

    Problem 140 with Pell's equation

--- p140.py
+++ p140.py
@@ -1,24 +1,21 @@
 # A(x)/x - A(x) = 1 + 3x + x*A(x) <=> A(x) = (3x^2+x) / (1-x-x^2)
 # A(x) = n <=> x = (sqrt(5n^2+14n+1)-n-1)/(2n+6) (n=2 first rational solution)
-# 5n2+14n+1 = m2 => n = (sqrt(196+20m2-20)-14)/10 = (sqrt(5m2+44)-7)/5
-# Also, A(x) = (3x+1)*B(x) where B(x) is from problem 137
-# Because 137 solutions are F7, F11, F15, ... fibonacci numbers... it's easy
+# With x=5n+7 we get x^2-44=y^2, http://www.numbertheory.org/php/binarygen.php
+from itertools import islice
 
 def gen():
-    p, q = 0, 1
+    F, G = -9, -4
+    C = [((-13, 25), (5, -13)),
+            ((13, 25), (5, 13)),
+            ((7, 5), (1, 7)),
+            ((-7, 5), (1, -7)),
+            ((8, 10), (2, 8)),
+            ((-8, 10), (2, -8))]
     while True:
-        yield (p,q)
-        p, q = p+q, p+2*q
+        for s in (-1,1):
+            for (a,b), (c,d) in C:
+                yield s*(a*F+b*G), s*(c*F+d*G)
+        F, G = 9*F+20*G, 4*F+9*G
 
-for p,q in gen():
-    if p>100: break
-    print(p,q)
-
-exit(1)
-
-sq = {i*i: i for i in range(1,1<<24)}
-for i in range(1,1<<24):
-    try:
-       s = sq[5*i*i+14*i+1]
-       print(i) #, s, (s-i-1) / (2*i+6))
-    except: pass
+N = sorted(islice(((i-7)//5 for i,j in gen() if i>7 and i%5==2), 50))
+print(sum(N[:30]))
