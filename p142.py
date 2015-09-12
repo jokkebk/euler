853112f40192d5c39135a0c9b9f4fbc4a8f1c69e
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2015-09-12 14:51:41 +0300

    Problem 142 LaTeX

--- p142.py
+++ p142.py
@@ -4,7 +4,7 @@ from util import pythag, square
 
 c, P = Counter(), defaultdict(list)
 
-for i,j,k in pythag(10**4, False):
+for i,j,k in pythag(999, False):
     c[i] += 1
     c[j] += 1
     P[i].append(j)
@@ -14,7 +14,8 @@ def gen():
     for r,num in c.most_common():
         if num<2: break
         for n,m in combinations(sorted(P[r]), 2):
-            if square(n*n+m*m+r*r) and not (n+m)&1:
-                yield (m*m+n*n+2*r*r)//2, (m*m+n*n)//2, (m*m-n*n)//2
+            if not (n+m)&1 and square(n*n+m*m+r*r):
+                yield (m*m+n*n+2*r*r)//2, 
+                    (m*m+n*n)//2, (m*m-n*n)//2
 
 print(sum(min(gen(), key=sum)))
