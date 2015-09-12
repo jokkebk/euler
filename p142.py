18800e2b473e7aa849fe2ebf102540f157686516
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2015-09-12 17:09:46 +0300

    Problem 142 slight minimization

--- p142.py
+++ p142.py
@@ -1,21 +1,19 @@
-from collections import Counter, defaultdict
+from collections import defaultdict
 from itertools import combinations
 from util import pythag, square
 
-c, P = Counter(), defaultdict(list)
+P = defaultdict(list)
 
 for i,j,k in pythag(999, False):
-    c[i] += 1
-    c[j] += 1
     P[i].append(j)
     P[j].append(i)
 
 def gen():
-    for r,num in c.most_common():
-        if num<2: break
+    for r in P:
+        if len(P[r]) < 2: continue
         for n,m in combinations(sorted(P[r]), 2):
             if not (n+m)&1 and square(n*n+m*m+r*r):
-                yield (m*m+n*n+2*r*r)//2, 
+                yield (m*m+n*n+2*r*r)//2, \
                     (m*m+n*n)//2, (m*m-n*n)//2
 
 print(sum(min(gen(), key=sum)))
