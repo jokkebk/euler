9ffe7e12d8cba86c3caad3c91a796f6d0124f1bf
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2015-09-30 16:05:29 +0300

    Problem 9 improved

--- p9.py
+++ p9.py
@@ -1,7 +1,7 @@
 import math, sys
 
-for a in range(0, 1000):
-    for b in range(0, 1000):
+for a in range(1, 500):
+    for b in range(a+1, 500):
         c = int(math.sqrt(a*a + b*b))
-        if c*c != a*a + b*b: continue
-        if a+b+c == 1000: print(a*b*c)
+        if c*c == a*a + b*b and a+b+c == 1000:
+            print(a*b*c)
