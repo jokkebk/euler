bd305ab614af0f519b1d32ff4f810dad8d2b69d7
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2016-06-19 21:50:27 +0300

    Problem 145 shorter

--- p145.py
+++ p145.py
@@ -1,14 +1,10 @@
-import timeit
-
-start = timeit.default_timer()
-
-rev = lambda n: int(str(n)[::-1])
 def isodd(n):
     while n:
         if not n&1: return False
         n //= 10
     return True
 
+rev = lambda n: int(str(n)[::-1])
 isok = lambda n: isodd(n + rev(n))
 
 semi = [n for n in range(10,100) if n%10]
@@ -24,17 +20,11 @@ for I in range(3):
             if isok(hi*div*10 + i*div + lo): ok += 1
         for i in range(100):
             n = hi*div*100 + i*div + lo
-            r = rev(n)
-            bot = n%div2 + r%div2
-            top = n//div2 + r//div2
-            if isodd(bot) and (isodd(top) or isodd(top+1)):
+            s = n+rev(n)
+            if isodd(s%div2) and isodd(s//(div2*10)):
                 semi2.append(n)
-                if isodd(n+r): ok += 1
+                if isodd(s): ok += 1
 
     semi, div = semi2, div2
 
 print(ok)
-
-stop = timeit.default_timer()
-
-print(stop - start )
